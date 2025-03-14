a9eca0078fd316a38a5b0d9ad2a64085356121fd
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-03-14 18:22:02 +0530

    chore: cleanup

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,14 @@
 import fs from 'node:fs/promises';
 import { parse } from 'yaml';
+import { rootConfigSchema } from './config-schema';
 
-async function parseYAMLConfig(filepath: string) {
+export async function parseYAMLConfig(filepath: string) {
 	const configFileContent = await fs.readFile(filepath, 'utf8');
 	const parsedConfig = parse(configFileContent);
 	return JSON.stringify(parsedConfig);
+}
+
+export async function validatedConfig(config: string) {
+	const validatedConfig = await rootConfigSchema.parseAsync(JSON.parse(config));
+	return validatedConfig;
 }
